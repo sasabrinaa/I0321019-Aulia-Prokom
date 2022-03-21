d779ff8ced05773bb81e12e157ac2606920e3d21
aulia <auliamaharani@student.uns.ac.id>
2022-03-21 13:49:10 +0700

    mengerjakan exercise 02

---
+++ Exercise-01/Untitled-1.py
@@ -0,0 +1,7 @@
+print("====================================")
+print("program bilangan integer")
+a=int(input("masukkan angka pertama ="))
+b=int(input("masukkan angka kedua   ="))
+jumlah = a+b
+print("nilai penjumlah=" , jumlah)
+print("====================================")
